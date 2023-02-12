020b952dd396021face6226e15c3f2b9cd260310
xzhang39 <xzhang39@tesla.com>
2023-02-12 11:11:54 -0800

    Add DoThenElse's test

--- null_any_test.go
+++ null_any_test.go
@@ -34,3 +34,35 @@ func TestNullAny_NewNullAny(t *testing.T) {
 	na := NewNullAny[int](1)
 	assert.False(t, na.IsNil())
 }
+
+func TestNullAny_DoThenElse(t *testing.T) {
+	na := &NullAny[int]{}
+	assert.True(t, na.IsNil())
+	cnt1 := 0
+	cnt2 := 0
+	na.DoThenElse(
+		func() {
+			cnt1++
+		},
+		func(i int) {
+			cnt2++
+		},
+	)
+	assert.Equal(t, 1, cnt1)
+	assert.Equal(t, 0, cnt2)
+
+	na = NewNullAny[int](1)
+	assert.False(t, na.IsNil())
+	cnt1 = 0
+	cnt2 = 0
+	na.DoThenElse(
+		func() {
+			cnt1++
+		},
+		func(i int) {
+			cnt2++
+		},
+	)
+	assert.Equal(t, 0, cnt1)
+	assert.Equal(t, 1, cnt2)
+}
